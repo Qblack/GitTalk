db19c3fadf32350d92b68003e35f00b13b5aebe4
Qblack <blac2410@mylaurier.ca>
2015-01-19 00:14:17 -0500

    Slide show scrolls but does not center

---
+++ SlideShow.js
@@ -0,0 +1,43 @@
+
+var windowHeight = $(window).height()
+
+var sections = $('.slide')
+
+sections.css("height", windowHeight);
+
+
+$(window).resize(function () {
+    "use strict";
+    var windowHeight = $(window).height(), windowWidth = $(window).width(), centerHeight = windowHeight / 2, centerWidth = windowWidth / 2;
+    sections.css("height", windowHeight);
+});
+
+var current = 0;
+function MoveUp() {
+    "use strict";
+    current -= 1;
+    current = Math.min(sections.length-1, Math.max(current, 0));
+
+    $("html, body").animate({ scrollTop: sections.eq(current).offset().top});
+}
+
+function MoveDown() {
+    "use strict";
+    current += 1;
+    current = Math.min(sections.length-1, Math.max(current, 0));
+    console.log(sections.eq(current));
+    $("html, body").animate({ scrollTop: sections.eq(current).offset().top});
+}
+
+document.onkeydown = function (evt) {
+    "use strict";
+    evt = evt || window.event;
+    switch (evt.keyCode) {
+        case 104: //8
+            MoveUp();
+            break;
+        case 98: //2
+            MoveDown();
+            break;
+    }
+};
